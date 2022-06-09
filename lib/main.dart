dce8040dba07d01991392b994e784f4b53e32f9a
Hoàng Thắng <hoangthang@Hoangs-Mac-mini.local>
2022-06-09 13:41:12 +0700

    update fire_base_option.dart and install cli for ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter_chat/screens/auth_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_chat/screens/chat_screen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
-      );
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     home: StreamBuilder(
       stream: FirebaseAuth.instance.authStateChanges(),
